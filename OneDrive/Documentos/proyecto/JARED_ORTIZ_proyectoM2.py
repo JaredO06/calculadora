4384e7192671ab5bd8972b3dec3e14dda12e8e7f
JaredO06 <otjared3@gmail.com>
2024-10-04 18:30:41 -0700

    Update JARED_ORTIZ_proyectoM2.py

--- OneDrive/Documentos/proyecto/JARED_ORTIZ_proyectoM2.py
+++ OneDrive/Documentos/proyecto/JARED_ORTIZ_proyectoM2.py
@@ -11,3 +11,28 @@ elif longitud < 4:
     print(f"Hacen falta letras. Solo tiene {longitud} letras.")
 else:
     print(f"Sobran letras. Tiene {longitud} letras.")
+
+
+
+
+
+
+
+
+# Programa para identificar el cuadrante de un punto
+x = float(input("Ingresa la coordenada X: "))
+y = float(input("Ingresa la coordenada Y: "))
+
+# Verificamos si alguna coordenada es 0
+if x == 0 or y == 0:
+    print("Ninguna coordenada puede ser 0.")
+else:
+    # Identificamos el cuadrante según los signos de X e Y
+    if x > 0 and y > 0:
+        print("El punto se encuentra en el cuadrante I.")
+    elif x < 0 and y > 0:
+        print("El punto se encuentra en el cuadrante II.")
+    elif x < 0 and y < 0:
+        print("El punto se encuentra en el cuadrante III.")
+    elif x > 0 and y < 0:
+        print("El punto se encuentra en el cuadrante IV.")
